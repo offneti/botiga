132f919b64c2491cdc279074f2bfb888b45b9353
usuari <usuari@DESKTOP-V3TFSO7>
2022-05-17 22:45:02 +0200

    cositas

--- Producte.java
+++ Producte.java
@@ -2,65 +2,55 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.botigapart1;
-import java.util.Scanner;
-import java.util.ArrayList;
-/**
- *
- * @author neti
- */
-public  class Producte {
-    
-    private float preu;
-    private String nom;
+import java.util.Objects;
 
-    public Producte(int preu, String nom) {
-        this.preu = preu;
-        this.nom = nom;
-    }
-    
-    public int getPreu() {
-        return (int) preu;
-    }
+public abstract class Producte {
+	private float preu;
+	private String nom;
 
-    public void setPreu(int preu) {
-        this.preu = preu;
-    }
+	public Producte(String nom, float preu) {
+		this.preu = preu;
+		this.nom = nom;
+	}
 
-    public String getNom() {
-        return nom;
-    }
+	public float getPreu() {
+		return preu;
+	}
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-    public float CalcularPreu( float quantitat){
-        return quantitat;
-    }
-   
-     public  static String afegirProducte() {
-        ArrayList<Producte> llistaProductes = new ArrayList<Producte>();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introdueix el nom del producte:");
-        String nom = sc.nextLine();
-        System.out.println("Introdueix el preu el producte");
-        int preu = sc.nextInt();
-         Producte p = new Producte(nom.charAt(preu), preu);
-        return afegirProducte();
-     }
-        
-       
-        
-            
-       
+	public void setPreu(float preu) {
+		this.preu = preu;
+	}
 
-        
-    
+	public String getNom() {
+		return nom;
+	}
 
-    @Override
-    public String toString() {
-        return "Producte{" + "preu=" + preu + ", nom=" + nom + '}';
-    }
-        
-    
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public abstract float calcularPreu(float quantitat);
+
+	@Override
+	public String toString() {
+		return "Producte Nom: " + this.nom + ", Preu: " + this.preu + "€. ";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nom, preu);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		Producte other = (Producte) obj;
+		return Objects.equals(nom, other.nom);
+	}
+	
+	
+
 }
